6b836653a135d4d596356609849c9390cb33c4d8
AlexanderBergeron <156931475+AlexanderBergeron@users.noreply.github.com>
2024-01-25 14:22:33 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const navLinkEls = document.querySelectorAll('.nav__link')
+const sectionEls = document.querySelectorAll('.section')
+
+let currentSection = 'home';
+window.addEventListener('scroll', () => {
+    sectionEls.forEach(sectionEl => {
+        if (window.scrollY >= (sectionEl.offsetTop - sectionEl,clientHeight / 3)){
+            currentSection = sectionEl.id;
+        }
+    });
+    navLinkEls.forEach(navLinkEl => {
+        if(navLinkEl.href.includes(currentSection)){
+            document.querySelector('.active').classList.remove('active');
+            navLinkEl.classList.add('active');
+        }
+    });
+});
